b57e6e55496acf252af2400d80fc65c51c22ff72
jisookim-kimchi <wltn249@gmail.com>
2026-05-10 18:18:23 +0200

    mariadb setup changed

--- srcs/mariadb/tools/setup.sh
+++ srcs/mariadb/tools/setup.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+if [ ! -d "/var/lib/mysql/mysql/$MYSQL_DATABASE" ]; then
 
  echo "DB init........."
  mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
 #init base db in Database 
     cat << EOF > /tmp/init.sql
-USE mysql;            
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 
 EOF
