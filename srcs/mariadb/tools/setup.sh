00f05dc8ac1196ab45ea62648ec0504d22cb2f65
Jisoo Kim <wlth249@gmail.com>
2026-05-10 22:07:53 +0200

    add try_files ~~~~

--- srcs/mariadb/tools/setup.sh
+++ srcs/mariadb/tools/setup.sh
@@ -6,16 +6,16 @@ if [ ! -d "/var/lib/mysql/mysql/$MYSQL_DATABASE" ]; then
  mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
 #init base db in Database 
-    cat << EOF > /tmp/init.sql
+mysql -u root << EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('$');
 FLUSH PRIVILEGES;
 
 EOF
 
-mysqld --user=mysql --bootstrap < /tmp/init.sql
+
 rm -f /tmp/init.sql
 
 fi
